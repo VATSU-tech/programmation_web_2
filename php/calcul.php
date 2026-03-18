dbcb6e052c36ce38d5ea898b26c678abf80c7558
VATSU_04 <vatsumalambo@gmail.com>
2026-03-18 09:53:23 +0200

    feat: use radio button to chose operation to do with php

--- php/calcul.php
+++ php/calcul.php
@@ -1,7 +1,17 @@
 <?php
 //recuperont cequi est dans le formulaire
-$nom = $_GET['nom'];
-$nom2 = $_GET['nom2'];
+$nom = $_POST['nom'];
+$nom2 = $_POST['nom2'];
 
-echo "la somme de ".$nom. " et ".$nom2. " est : ".($nom+$nom2);
+$signe = $_POST['signe'];
+
+if($signe == "addition") {
+    echo " \n la somme de ".$nom. " et ".$nom2. " est : ".($nom+$nom2);
+} elseif ($signe == "sousstraction") {
+    echo " \n la difference de ".$nom. " et ".$nom2. " est : ".($nom-$nom2);
+} else {
+    echo "Veuillez choisir une opération.";
+}
+
+// echo " \n la somme de ".$nom. " et ".$nom2. " est : ".($nom+$nom2);
 ?>
