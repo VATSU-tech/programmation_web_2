a3d8d5b668bb826ec765429fa4fa696d481f23bf
VATSU_04 <vatsumalambo@gmail.com>
2026-03-18 10:00:23 +0200

    feat: operations with  : - addition - soustraction - multiplication - division - modulo

--- php/calcul.php
+++ php/calcul.php
@@ -9,6 +9,12 @@ if($signe == "addition") {
     echo " \n la somme de ".$nom. " et ".$nom2. " est : ".($nom+$nom2);
 } elseif ($signe == "sousstraction") {
     echo " \n la difference de ".$nom. " et ".$nom2. " est : ".($nom-$nom2);
+} elseif ($signe == "division") {
+    echo " \n la division de ".$nom. " et ".$nom2. " est : ".($nom/$nom2);
+} elseif ($signe == "multiplication") {
+    echo " \n la multiplication de ".$nom. " et ".$nom2. " est : ".($nom*$nom2);
+} elseif ($signe == "modulo") {
+    echo " \n le modulo de ".$nom. " et ".$nom2. " est : ".($nom%$nom2);
 } else {
     echo "Veuillez choisir une opération.";
 }
