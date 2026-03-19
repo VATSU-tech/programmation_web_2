c6b188c21a35a43f4bd5e36e0f61ec69a7ba57d9
VATSU_04 <vatsumalambo@gmail.com>
2026-03-19 13:18:02 +0200

    feat: insertion avec requette preparer

--- php/connection/post/insertion.php
+++ php/connection/post/insertion.php
@@ -4,7 +4,11 @@ if(isset($_POST["nom"]) && isset($_POST["categorie"]) && isset($_POST["pu"])){
     $nom = $_POST["nom"];
     $categorie = $_POST["categorie"];
     $pu = $_POST["pu"];
-    $requete = $connection->exec("INSERT INTO produit (nom, categorie, pu) VALUES ('$nom', '$categorie', $pu)");
-    // $requete->exec(array($nom, $categorie, $pu));
+    //premiere methode d'insertion NB: deconseillée car elle est sujette à des attaques d'injection SQL
+    // $requete = $connection->exec("INSERT INTO produit (nom, categorie, pu) VALUES ('$nom', '$categorie', $pu)");
+
+    //deuxieme methode d'insertion NB: recommandée car elle utilise des requetes préparées et est moins sujette à des attaques d'injection SQL
+    $requet = $connection->prepare("INSERT INTO produit (nom, categorie, pu) VALUES (?, ?, ?)");
+    $requet->execute(array($nom, $categorie, $pu));
 }
 ?>
