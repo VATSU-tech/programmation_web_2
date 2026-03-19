6ca48dccf07c8a0e6ba43c36f929c7239aa3873b
VATSU_04 <vatsumalambo@gmail.com>
2026-03-19 13:07:15 +0200

    ajouter les donnee a la DB

--- php/connection/post/insertion.php
+++ php/connection/post/insertion.php
@@ -1,3 +1,10 @@
 <?php
-
+include ("./connection.php");
+if(isset($_POST["nom"]) && isset($_POST["categorie"]) && isset($_POST["pu"])){
+    $nom = $_POST["nom"];
+    $categorie = $_POST["categorie"];
+    $pu = $_POST["pu"];
+    $requete = $connection->prepare("INSERT INTO produit (nom, categorie, pu) VALUES (?, ?, ?)");
+    $requete->execute(array($nom, $categorie, $pu));
+}
 ?>
