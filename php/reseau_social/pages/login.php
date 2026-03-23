ce0f28f9f724dcff2b607c36b4d0697becfa9f3b
VATSU_04 <vatsumalambo@gmail.com>
2026-03-23 16:12:26 +0200

    design: ameliorer la page de connection

--- php/reseau_social/pages/login.php
+++ php/reseau_social/pages/login.php
@@ -3,13 +3,12 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../../style/index.css">
-    <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> -->
+    <link rel="stylesheet" href="../../../style/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
         integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>cookies</title>
+    <title>log in</title>
 </head>
 
 <body>
@@ -25,6 +24,7 @@
                 <div class="buttons">
                     <button name="envoyer">Se connecter</button>
                 </div>
+                <span>pas de compte <a href="signup.php">S'inscrire</a></span>
         </form>
 
         <?php
@@ -32,7 +32,7 @@
 
     </main>
     <footer>
-        <a href="https://github.com/VATSU-tech/programmation_web_2">&COPY; VATSU-tech <i class="fa-brands fa-github"></i></a>
+        <a class="git" href="https://github.com/VATSU-tech/programmation_web_2">&COPY; VATSU-tech <i class="fa-brands fa-github"></i></a>
     </footer>
 </body>
 
