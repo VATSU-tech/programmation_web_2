48b687cded8b6fdbcef5d19e4223cb8a7480ca6a
VATSU_04 <vatsumalambo@gmail.com>
2026-03-23 11:58:08 +0200

    feat: initialiser les sessions

--- php/cookies/login.php
+++ php/cookies/login.php
@@ -1,6 +1,13 @@
 <?php
-// sesssion_start();
+session_start();
 if(isset($_POST["nom"]) && isset($_POST["password"])){
-    $userName = $_POST["nom"];
+    $username = $_POST["nom"];
     $password = $_POST["password"];
+
+    if($username == "admin" && $password == "1234"){
+        $_SESSION["username"] = $username;
+        
+        header('Location: ./dashboard.php');
+    }else echo'Mot de passe ou nom d\'utilisateur incorect';
+
 } else echo"Compteler touts les champs";
