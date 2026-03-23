f19094761877565124e29de1a6a6c6f4a4e24dd1
VATSU_04 <vatsumalambo@gmail.com>
2026-03-23 17:31:30 +0200

    create: la page d'acceuil pour le site

---
+++ php/reseau_social/pages/dashboard.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../../../style/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>log in</title>
+</head>
+
+<body>
+    <header>
+        <h1>DASHBOARD</h1>
+    </header>
+    <main>
+        <h1>Bienvenue user</h1>
+    </main>
+    <footer>
+        <a class="git" href="https://github.com/VATSU-tech/programmation_web_2">&COPY; VATSU-tech <i class="fa-brands fa-github"></i></a>
+    </footer>
+</body>
+
+</html>
